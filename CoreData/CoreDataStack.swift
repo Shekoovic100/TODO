11117af3af105722aed898ed011f841d597e62c4
Sherif Samy <sherif.samy7770@gmail.com>
2023-01-04 16:04:41 +0200

    Enhancements in core data stack

--- CoreData/CoreDataStack.swift
+++ CoreData/CoreDataStack.swift
@@ -15,19 +15,20 @@ import CoreData
 class CoreDataStack {
     
     
+    static let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    static let manageContext = appDelegate?.persistentContainer.viewContext
+    
     // save data
     
     static func saveData(todo:getToDo){
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{return}
-        let manageContext = appDelegate.persistentContainer.viewContext
-        guard let Entity = NSEntityDescription.entity(forEntityName: "ToDo", in: manageContext) else {return}
+ 
+        guard let Entity = NSEntityDescription.entity(forEntityName: "ToDo", in: manageContext!) else {return}
         let toDoObject = NSManagedObject.init(entity: Entity, insertInto: manageContext)
         
         toDoObject.setValue(todo.list, forKey: "list")
         
         do {
-            try manageContext.save()
+            try manageContext!.save()
             print("success")
         }catch{
             print("Error !!!")
@@ -39,13 +40,11 @@ class CoreDataStack {
     static func getToDO()->[getToDo] {
         
        var  toDos:[getToDo] = []
-        
-       guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return [] }
-       let context = appDelegate.persistentContainer.viewContext
+       
        let fetchData = NSFetchRequest<NSFetchRequestResult>(entityName: "ToDo")
         
         do {
-            let result  = try context.fetch(fetchData) as! [NSManagedObject]
+            let result  = try manageContext?.fetch(fetchData) as! [NSManagedObject]
             
             for todo in result {
                 let list = todo.value(forKey: "list") as? String
@@ -62,15 +61,14 @@ class CoreDataStack {
     
     static func deleteToDo(index:Int){
         
-        guard let appDelegate  = UIApplication.shared.delegate as? AppDelegate else {return}
-        let context = appDelegate.persistentContainer.viewContext
+      
         let fetchdata = NSFetchRequest<NSFetchRequestResult>(entityName: "ToDo")
         
         do {
-            let result = try context.fetch(fetchdata) as! [NSManagedObject]
+            let result = try manageContext?.fetch(fetchdata) as! [NSManagedObject]
             let deleteToDo = result[index]
-            context.delete(deleteToDo)
-            try context.save()
+            manageContext?.delete(deleteToDo)
+            try manageContext?.save()
         }catch{
             print("Error !!!!")
         }
